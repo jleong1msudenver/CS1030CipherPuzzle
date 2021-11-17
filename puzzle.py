55ee5722a04240f906debdda85c1a5e5416461f2
jleong1msudenver <jleong1@msudenver.edu>
2021-11-16 23:30:39 -0500

    initial commit

---
+++ puzzle.py
@@ -0,0 +1,46 @@
+# DRO BOCMEO WSCCSYX GSVV ECO K ROVSMYZDOB,
+# THE  E 
+# KBBSFSXQ KD XYYX DYWYBBYG.
+
+# LO BOKNI DY LBOKU YED KC CYYX KC IYE ROKB DRBOO LVKCDC YX K GRSCDVO.
+#  E  E         E
+# S'VV LO GOKBSXQ K BON KBWLKXN.
+#       E  E         E    
+
+A, B = 'x', 'x'
+# C = 'x'
+# D = 'x'
+# E = 'x'
+# F = 'x'
+# G = 'x'
+# H = 'x'
+# I = 'x'
+# J = 'x'
+# K = 'x'
+# L = 'x'
+# M = 'x'
+# N = 'x'
+# O = 'x'
+# P = 'x'
+# Q = 'x'
+# R = 'x'
+# S = 'x'
+# T = 'x'
+# U = 'x'
+# V = 'x'
+# W = 'x'
+# X = 'x'
+# Y = 'x'
+# Z = 'x'
+print(A + B)
+
+
+# print(DRO, BOCMEO, WSCCSYX, GSVV, ECO, K, ROVSMYZDOB)
+# KBBSFSXQ KD XYYX DYWYBBYG.
+# LO BOKNI DY LBOKU YED KC CYYX KC IYE ROKB DRBOO LVKCDC YX K GRSCDVO.
+# SVV LO GOKBSXQ K BON KBWLKXN.
+
+
+
+
+# Which is thje cypher?
